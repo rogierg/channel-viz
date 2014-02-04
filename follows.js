64242f19b2cf6b2f5c4d3dee95ef20655dfdab8e
rogierg <rogierg@hotmail.com>
2014-02-04 15:51:12 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,7 +16,7 @@
 		defaultFeeds	= [1116617964], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Energy Monitor', // Replaces Xively logo in the header
 		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 900, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
